1b11ced52de8446f2aaf3b92f915a7b7cb6436b9
Sam <sam@Sams-MacBook-Pro.local>
2014-09-02 00:03:56 -0400

    Started building basic behaviours for ColourTarget class.

--- js/plugin.js
+++ js/plugin.js
@@ -1,26 +1,66 @@
-function ColourTarget (){
-	
+function ColourTarget (node){
+	this.$ = $(node);
+	this.initColours = {};
+	this.hoverColours = {};
 }
 
+ColourTarget.prototype = {
+	constructor: ColourTarget,
+	init: function(){
+		var self = this;
+		self.getInitColours();
+		self.initBehaviour();
+	},
+	getInitColours: function(){
+		this.initColours = {
+			'color': this.$.css('color'),
+			'border-color': this.$.css('border-color'),
+			'background-color': this.$.css('background-color'),
+			'text-shadow-color': this.$.css('text-shadow-color'),
+			'box-shadow-color': this.$.css('box-shadow-color')
+		};
+	},
+	getHoverColours: function(){
+		this.hoverColours = {
+			'color': this.$.css('color'),
+			'border-color': this.$.css('border-color'),
+			'background-color': this.$.css('background-color'),
+			'text-shadow-color': this.$.css('text-shadow-color'),
+			'box-shadow-color': this.$.css('box-shadow-color')
+		};
+	},
+	initBehaviour: function(){
+		var self = this;
+		self.$.on('mouseenter', function(){
+			self.getHoverColours();
+			self.$.css('outline', 'red dotted 1px');
+			self.$.on('click', function(e){
+				e.stopImmediatePropagation();
+				$(this).ColorPickerSliders({
+		            order: {
+		                cie: 1,
+		                preview: 2
+		            },
+		            color: self.initColours.color,
+		            onchange: function(container, color){
+		            	self.$.css('color', color.tiny.toString());
+		            }
+		        });
+			})
+		});
 
-$('*').on('mouseenter', function(){
-	$(this).css('outline', 'red dotted 1px');
-	$(this).on('click', function(e){
-		e.stopImmediatePropagation();
-		var colortarget = this;
-		$(this).ColorPickerSliders({
-            order: {
-                cie: 1,
-                preview: 2
-            },
-            color: $(this).css('color'),
-            onchange: function(container, color){
-            	this.connectedinput.css('color', color.tiny.toString());
-            }
-        });
-	})
-});
+		self.$.on('mouseleave', function(){
+			self.$.css('outline', 'none');
+		});
+	},
+	setColours: function(){
+		var self = this
+		$.each(this.initColours, function(a,b){
+			self.$.css(a,b);
+		});
+	}
+}
 
-$('*').on('mouseleave', function(){
-	$(this).css('outline', 'none');
+$('html *').each(function(){
+	var obj = new ColourTarget(this).init();
 });
